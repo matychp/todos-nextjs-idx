712044338131cf43f43927dee9d10ec4532231d5
matychp <matychp@gmail.com>
2025-02-09 18:04:30 +0000

    ✨ Delete a single todo

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,10 @@ export default function Home() {
     }
   };
 
+  const handleDeleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       handleAddTodo();
@@ -81,6 +85,16 @@ export default function Home() {
               >
                 {todo.text}
               </label>
+               <button
+                onClick={() => handleDeleteTodo(todo.id)}
+                className="text-red-500 hover:text-red-700 focus:outline-none ml-4"
+              >
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+                
+              </button>
+
             </div>
           </li>
         ))}
